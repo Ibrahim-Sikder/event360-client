cfc8e2a48f63812933a73b1c83a7cea0af8d7745
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-01-28 03:43:36 +0600

    service-section-completed

---
+++ src/pages/Home/ServiceSection.tsx
@@ -0,0 +1,179 @@
+import Container from "../../ui/Container"
+import event from "../../assets/images/event6.png"
+import { HiOutlineCheck } from "react-icons/hi"
+const ServiceSection = () => {
+
+  return (
+    <div className="bg-[#0F172A] py-8">
+      <Container className="">
+        <div className="text-white max-w-[600px] mx-auto text-center ">
+          <h2 className="text-4xl font-bold ">Our Service </h2>
+          <p>
+            Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
+            ornare viverra. Ultrices faucibus neque velit risus ac id lorem.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 mt-[54px] ">
+          <div className="text-white bg-[#01000D]  p-4 w-[400px] h-[569px] mx-auto flex flex-col  items-center  ">
+            <div>
+              <img
+                className="w-[362px] h-[253px] object-cover "
+                src={event}
+                alt=""
+              />
+              <div>
+                <h4 className="text-3xl font-bold mt-3 mb-2">
+                  Corporate event
+                </h4>
+                <div className="space-y-5 ">
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>{" "}
+                    One day pas access all lecture
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Lunch and Snack{" "}
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Meet Event Speaker
+                  </div>
+
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Front Seat
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    One day pas access all lecture
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          
+
+          <div className="text-white bg-[#01000D]  p-4 w-[400px] h-[569px]  flex flex-col  items-center ">
+
+         
+
+
+
+            <div>
+             <div>
+             <h4 className="text-3xl font-bold mt-3 mb-2">
+             Wedding event
+                </h4>
+                <p className="max-w-[38ch] leading-7 mb-5  ">
+                Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut posuere felis arcu tellus tempus in in ultricies.Ut posuere felis arcu tellus tempus in in ultricies.
+                </p>
+             </div>
+              <div>
+               
+                <div className="space-y-5 ">
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>{" "}
+                    One day pas access all lecture
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Lunch and Snack{" "}
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Meet Event Speaker
+                  </div>
+
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Front Seat
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    One day pas access all lecture
+                  </div>
+                </div>
+              </div>
+              <button className="bg-[#01DAF8] w-[362px] h-[52px] text-black mt-5">View Details</button>
+            </div>
+          </div>
+
+          <div className="text-white bg-[#01000D]  p-4 w-[400px] h-[569px] mx-auto flex flex-col  items-center  ">
+            <div>
+              <img
+                className="w-[362px] h-[253px] object-cover "
+                src={event}
+                alt=""
+              />
+              <div>
+                <h4 className="text-3xl font-bold mt-3 mb-2">
+                Organization event
+                </h4>
+                <div className="space-y-5 ">
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>{" "}
+                    One day pas access all lecture
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Lunch and Snack{" "}
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Meet Event Speaker
+                  </div>
+
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    Front Seat
+                  </div>
+                  <div className="flex items-center ">
+                    <span className="mr-2 w-4 h-4 text-center bg-red-800 rounded-full ">
+                      <HiOutlineCheck className="mr-2 text-[#00F6E7]" />
+                    </span>
+                    One day pas access all lecture
+                  </div>
+
+
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default ServiceSection
