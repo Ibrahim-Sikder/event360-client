8ff0953b9bb5b7625331e869e6a2edf583865204
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-01-28 18:19:06 +0600

    event-section-price-section-responsive-complete

--- src/pages/Home/PriceSection.tsx
+++ src/pages/Home/PriceSection.tsx
@@ -9,8 +9,8 @@ const PriceSection = () => {
                     <span>pricing</span>
                     <h3 className="text-[64px] font-bold">Package Pricing </h3>
                 </div>
-                <div className="grid grid-cols-3 place-items-center mt-14 ">
-                    <div className="w-full max-w-[373px] h-[530px] p-5 bg-[#11151E] rounded-md pl-10 pt-10 pb-10   flex flex-col justify-between ">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center mt-14 ">
+                    <div className="priceCard">
                         <div>
                         <b className="uppercase">standart</b>
                         <h3 className="text-4xl font-bold mt-5">$120</h3>
@@ -32,10 +32,10 @@ const PriceSection = () => {
                         </div>
 
                         
-                       <button className="bg-[#242830] w-[277px] h-[52px] text-white   ">Get Your Ticket </button>
+                       <button className="btn2 ">Get Your Ticket </button>
 
                     </div>
-                    <div className="w-full max-w-[373px] h-[557px] p-5 bg-[#0038B4] rounded-md pl-10 pt-10 pb-10   flex flex-col justify-between -mt-5 ">
+                    <div className="priceCard bg-[#0038B4] ">
                         <div>
                         <b className="uppercase">Premium </b>
                         <h3 className="text-4xl font-bold mt-5">$180</h3>
@@ -62,10 +62,10 @@ const PriceSection = () => {
                         </div>
 
                         
-                       <button className="bg-[#00F8E6] w-[277px] h-[52px] text-white   ">Get Your Ticket </button>
+                       <button className="btn2 bg-[#00F8E6] ">Get Your Ticket </button>
 
                     </div>
-                    <div className="w-full max-w-[373px] h-[530px] p-5 bg-[#11151E] rounded-md pl-10 pt-10 pb-10   flex flex-col justify-between ">
+                    <div className="priceCard">
                         <div>
                         <b className="uppercase">basic</b>
                         <h3 className="text-4xl font-bold mt-5">$120</h3>
@@ -87,7 +87,7 @@ const PriceSection = () => {
                         </div>
 
                         
-                       <button className="bg-[#242830] w-[277px] h-[52px] text-white   ">Get Your Ticket </button>
+                       <button className="btn2">Get Your Ticket </button>
 
                     </div>
 
